bffa288b4873233e3523af0079317aae296238a8
Gajo Petrovic <gajop@uns.ac.rs>
2013-01-16 14:33:46 +0100

    added beautiful soup, parsing files by topics

--- bin/crawler.py
+++ bin/crawler.py
@@ -3,15 +3,24 @@ import re
 import mechanize
 import subprocess
 import os
+import sys
+#import cPickle as Serializer
+import json as Serializer
+from urlparse import urljoin
 
 start_year = 2006
 end_year = 2012
-links = None
-text = None
 
 SCRIPT_PATH = os.path.realpath(__file__)
-SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
-DATA_DIR = os.path.realpath(SCRIPT_DIR + "/../data/")
+SCRIPT_DIR = os.path.dirname(SCRIPT_PATH) + "/"
+
+sys.path.insert(0, os.path.realpath(SCRIPT_DIR + "../beautifulsoup4-4.1.3/"))
+from bs4 import BeautifulSoup
+
+DATA_DIR = os.path.realpath(SCRIPT_DIR + "/../data") + "/"
+
+br = mechanize.Browser()
+br.set_proxies({"http": "proxy.uns.ac.rs:8080", })
 
 def makeDirs():
     if not os.path.exists(DATA_DIR):
@@ -21,32 +30,73 @@ def makeDirs():
         if not os.path.exists(yearDir):
             os.mkdir(yearDir)
         
+def getArticlesFromPage(docListing):
+    soup = BeautifulSoup(docListing)
+    soup = BeautifulSoup(soup.prettify())
+    aTags = soup.find_all("a")
+    aTags = [aTag for aTag in aTags if re.match(r"pdf/\d+\.pdf", aTag.get("href"))]
 
-def getDocListingOnPage(br):
-    url = br.geturl()
-    response = mechanize.urlopen(url)
-    return response.read()
+    articles = []
+    for aTag in aTags:
+        authors = aTag.parent.previous_sibling.previous_sibling.previous_sibling.previous_sibling.text
+        authors = authors.strip()
+        title = aTag.text.strip()
+        pdfName = re.sub("pdf/(\d+)\.pdf", r"\1.pdf", aTag.get("href"))
 
-def grabAllDocs():
-    br = mechanize.Browser()
-    br.set_proxies({"http": "proxy.uns.ac.rs:8080", })
+        article = {"authors":authors, "title":title, "pdfName":pdfName, "url":aTag.get("href")}
+        articles.append(article)
+
+    return articles
+
+
+def grabAllListings():
     baseURL = "http://www.e-drustvo.org/proceedings/YuInfo$YEAR/html/proceedings.html"
     for year in range(start_year, end_year + 1):
+        yearDir = DATA_DIR + str(year) + "/"
         url = baseURL.replace("$YEAR", str(year))
         [links, text] = getTopicsByYear(year)
         #currentPage = "http://www.e-drustvo.org/proceedings/YuInfo" + str(year) + "/html/search.html"
         br.open(url)
         #response = mechanize.urlopen(currentPage)
         #print response.read()
-        for link in br.links():
+        brLinks = [link for link in br.links()]
+        for link in brLinks:
+#            http://www.e-drustvo.org/proceedings/YuInfo2006/html/simulation.html
             if link.url in links:
-                br.follow_link(link)
-                docListing = getDocListingOnPage(br)
-                yearDir = DATA_DIR + str(year) + "/"
-                f = open(yearDir + "listing.html", "w")
+                print link.url + " year : " + str(year)
+                topic = link.url.split('.htm')[0]
+
+                topicDir = yearDir + topic + "/"
+                if not os.path.exists(topicDir):
+                    os.mkdir(topicDir)
+
+                docListing = br.follow_link(link).read()
+                filePath = yearDir + topic + "-listing.html"
+                f = open(filePath, "w")
                 f.write(docListing)
                 f.close()
+
+                articles = getArticlesFromPage(docListing)
+                f = open(yearDir + topic + "-meta.txt", "w")
+                serializedArticles = Serializer.dumps(articles)
+                f.write(serializedArticles)
+                f.close()
+
+                listingURL = br.geturl()
+                for article in articles:
+                    link = urljoin(listingURL, article["url"])
+                    print "Fetching file... " + link
+                    try:
+                        pdfContent = mechanize.urlopen(link).read()
+                        f = open(topicDir + article["pdfName"], "w")
+                        f.write(pdfContent)
+                        f.close()
+                    except:
+                        print "Failure fetching file " + link
+
                 br.back()
+
+
 #        for link in br.links(url_regex="pdf"):
 #            print link
         #      # takes EITHER Link instance OR keyword args
@@ -58,7 +108,8 @@ def grabProceedings():
     for year in range(start_year, end_year + 1):
         url = baseURL.replace("$YEAR", str(year))
         response = mechanize.urlopen(url)
-        f = open(DATA_DIR + str(year) + "-proceedings", "w")
+        filePath = DATA_DIR + str(year) + "-proceedings.html"
+        f = open(filePath, "w")
         f.write(response.read())
         f.close()
 
@@ -70,7 +121,8 @@ def getTopicsByYear(year):
     for link in linkStr.split('\n'):
         link = link.strip()
         if len(link) > 0:
-            links.append(link)            
+            links.append(link)
+
     for text in textStr.split('\n'):
         text = text.strip()
         if len(text) > 0:
@@ -79,4 +131,4 @@ def getTopicsByYear(year):
     
 makeDirs()
 grabProceedings()
-grabAllDocs()
+grabAllListings()
--- bin/get-fields.sh
+++ bin/get-fields.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATA_DIR="$SCRIPT_DIR/../data/"
-echo $DATA_DIR
-proceedings="20*-proceedings"
+if [[ -e "$DATA_DIR" ]]; then
+    DATA_DIR="../data/";
+fi
+proceedings="20*-proceedings.html"
 if [[ -n "$2" ]]; then
-    proceedings="$2-proceedings"
+    proceedings="$2-proceedings.html"
 fi
 html=`grep "oblasti" -i "$DATA_DIR"$proceedings -A20 | grep htm`
-#echo "$html"
 if [ "$1" == "links" ]; then
     links=`echo "$html" | sed -e 's/.*<a href="//g' | sed -e 's/".*//g'`
     echo "$links"
